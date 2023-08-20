83da3d0ac63f0028f1ebc8d199afdff458bef3ae
CuriousnTT <trygve.taranger2@gmail.com>
2023-08-20 13:22:14 +0200

    Islated alchemy to one file

--- src/main.py
+++ src/main.py
@@ -1,17 +1,13 @@
 #Dependencies
-import subprocess, uvicorn
-from sqlalchemy import text, MetaData, Table, Column, Integer, String
+import uvicorn
+from pmalchemy.alchemy import clear_database, make_database, show_all_tables
 
 if __name__ == "__main__":
 
     #Set up SQL Database
-    #DATABASE_PATH="pmdb/pmdb.sqlite3"
-    #SOURCE="pmdb/pmdb.sql"
-    ALCHEMY_PATH=".\src\pmalchemy\alchemy.py"
-    run_sqlalchemy = f"python {ALCHEMY_PATH}"
-    subprocess.call(run_sqlalchemy)
-    #build_db_tables = f"sqlite3 {DATABASE_PATH} --init  {SOURCE}; .exit"
-    #subprocess.call(build_db_tables)
+    #show_all_tables()
+    #clear_database()
+    #make_database()
 
     #Set up API
     uvicorn.run("api:app", host="127.0.0.1", reload=True)
--- src/pmalchemy/alchemy.py
+++ src/pmalchemy/alchemy.py
@@ -2,22 +2,30 @@ import sqlalchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Table, Integer, String, Column, MetaData
 
+DATABASE_PATH = "pmdb/pmdb.sqlite3"
+
 class Base(DeclarativeBase):
     pass
 
-engine = sqlalchemy.create_engine("sqlite+pysqlite:///:memory:", echo=True)
+engine = sqlalchemy.create_engine(f"sqlite+pysqlite:///{DATABASE_PATH}", echo=True)
 metadata_obj = Base.metadata
-generation = Table(
-        "generation",
-        metadata_obj,
-        Column("genNr", Integer)
-    )
-pm_dex = Table(
-        "pmDex",
-        metadata_obj,
-        Column("pmDexNr", Integer, primary_key=True),
-        Column("pmName", String(45), unique=True),
-        Column("gender", Integer),
-    )
+#generation = Table(
+#        "generation",
+#        metadata_obj,
+#        Column("genNr", Integer)
+#    )
+#pm_dex = Table(
+#        "pmDex",
+#        metadata_obj,
+#       Column("pmDexNr", Integer, primary_key=True),
+#        Column("pmName", String(45), unique=True),
+#        Column("gender", Integer),
+#    )
+def show_all_tables():
+    Base.metadata.reflect(bind=engine)
 
-Base.metadata.create_all(engine)
+def clear_database():
+    Base.metadata.drop_all(bind=engine)
+
+def make_database():
+    Base.metadata.create_all(engine)
