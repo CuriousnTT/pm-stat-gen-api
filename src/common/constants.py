6fc7ccc4375c1f18296913c6e4c52276a009e387
Lupu2 <35259974+Lupu2@users.noreply.github.com>
2023-09-12 21:53:39 +0200

    added more desired columns

--- src/common/constants.py
+++ src/common/constants.py
@@ -8,5 +8,7 @@ class Constants(Enum):
     "Move 1", "Move 2", "Move 3", "Move 4",
     "HP Stat", "Attack Stat", "Defense Stat", "Special Stat", "Special Attack Stat", "Special Defense Stat", "Speed Stat",
     "Attack DV", "Defense DV", "Special DV", "Speed DV",
-    "HP IV", "Attack IV", "Defense IV", "Special Attack IV", "Special Defense IV", "Speed IV"
+    "HP IV", "Attack IV", "Defense IV", "Special Attack IV", "Special Defense IV", "Speed IV",
+    "HP Stat Experience", "Attack Stat Experience", "Defense Stat Experience", "Special Stat Experience" "Special Attack Stat Experience", "Special Defense Stat Experience", "Speed Stat Experience",
+    "HP EV", "Attack EV", "Defense EV", "Special Attack EV", "Special Defense EV", "Speed EV"
 ]
